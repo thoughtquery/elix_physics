7d73b48fd26e6fb57ceb6fbd22b3a646a8ec83c7
Andrew Benavides <thoughtquery@gmail.com>
2016-11-19 23:39:56 -0800

    Non-idiomatic elixir, prepare to refactor

--- lib/physics/rocketry.ex
+++ lib/physics/rocketry.ex
@@ -1,9 +1,19 @@
 defmodule Physics.Planet do
+@moduledoc """
+           g    = Gravitational Constant
+           gmr  = Grav Const, Mass, Radius
+           vms  = Velocity Meters per Second
+           vkms = Velocity Kilometers per Second
+           """
+           
+
   defstruct [
     name: "Earth",
     radius_m: 6.371e6,
     mass_kg:  5.97e24
 ]
+
+
   def escape_velocity do
     g    = 6.67e-11
     gmr  = 2 * g * planet.mass_kg/planet.radius_m
@@ -14,6 +24,7 @@ defmodule Physics.Planet do
 
 end
 
+v = %Physics.Planet{} |> Physics.Planet.escape_velocity 
 
 defmodule Physics.Rocketry do
 
